ab766802113c81232fff348dd2f053e8f012ff2f
GRIDAR L S <gridar15705@gmail.com>
2026-07-06 21:08:54 +0530

    Updated Variables.java

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Gridar";
+        int age = 20;
+        double height = 176.0;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
