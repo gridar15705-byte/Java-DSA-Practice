5e6b83243d48fb7fcc7db18ba5f2e2a9cf6875dd
GRIDAR L S <gridar15705@gmail.com>
2026-07-10 10:09:54 +0530

    Added Largest Element program

---
+++ LargestElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+
+        sc.close();
+    }
+}
