07ebb7d40bc8f71f18033e1e2077cbd7e9444d3b
GRIDAR L S <gridar15705@gmail.com>
2026-07-10 09:53:04 +0530

    Added Forloop program

---
+++ Forloop.java
@@ -0,0 +1,12 @@
+public class Forloop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Numbers from 1 to 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
