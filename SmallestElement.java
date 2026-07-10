c57c7a8a2a2c338bc19c9ef141bcc2f8086e20a4
GRIDAR L S <gridar15705@gmail.com>
2026-07-10 10:14:46 +0530

    Added Smallest Element program

---
+++ SmallestElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SmallestElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest Element = " + smallest);
+
+        sc.close();
+    }
+}
