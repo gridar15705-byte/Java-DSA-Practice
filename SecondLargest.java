7e00325ed23563d85f239526862490eccfd13959
GRIDAR L S <gridar15705@gmail.com>
2026-07-10 21:43:16 +0530

    Added Second largest progran

---
+++ SecondLargest.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SecondLargest{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+        System.out.println("Second Largest Element = " + secondLargest);
+
+        sc.close();
+    }
+}
