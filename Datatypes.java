e5a9ebd7daeae0671fa1c811113831347f20f677
GRIDAR L S <gridar15705@gmail.com>
2026-07-10 09:21:50 +0530

    Added Datatypes program

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+public class Datatypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double cgpa = 8.75;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Gridar";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
