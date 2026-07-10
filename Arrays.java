df51bb4d21fc270269e107bcb4d5f867f8b49727
GRIDAR L S <gridar15705@gmail.com>
2026-07-10 10:02:18 +0530

    Added Array program

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First Element: " + numbers[0]);
+        System.out.println("Second Element: " + numbers[1]);
+        System.out.println("Third Element: " + numbers[2]);
+        System.out.println("Fourth Element: " + numbers[3]);
+        System.out.println("Fifth Element: " + numbers[4]);
+    }
+}
